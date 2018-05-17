232da78669c21677da89817ce805e8c0bf1b7103
Chaikin Evgenii <fat_hack@rambler.ru>
2018-05-17 14:56:10 +0700

    Доработана форма авторизации

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Пользователь с таким E-Mail адресом не найден.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
