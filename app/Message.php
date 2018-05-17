1d7888e6f4012d6f65983b6c2f74edb328887f3c
Chaikin Evgenii <fat_hack@rambler.ru>
2018-05-17 13:50:32 +0700

    Обновлен PHPDoc в моделях

--- app/Message.php
+++ app/Message.php
@@ -4,6 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Message
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property int $user_id
+ * @property string $message
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Message whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Message whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Message whereMessage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Message whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Message whereUserId($value)
+ */
 class Message extends Model
 {
     protected $table = 'messages';
